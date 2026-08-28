0832b3d1fe719d6040aaa1e2804a7618967eb0b2
guspelepe <gustavopelepe2005@gmail.com>
2026-08-27 21:48:11 -0300

    Maps

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-    GOOGLE_MAPS_API_KEY: 'AIzaSyCoIVcV-wKiWWIOKsQl75u0RjfyAqvQKwc' // SUA CHAVE AQUI
+    GOOGLE_MAPS_API_KEY: 'AIzaSyCp5cnooHUhNpohg1RdeJBMZ-Ox9jH3iyk'
 };
